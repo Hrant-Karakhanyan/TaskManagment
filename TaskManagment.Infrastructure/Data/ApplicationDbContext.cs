d455e3b6cf4bea9636c93940bdfcf892ac48aea5
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-09 08:17:50 -0800

    ApplicatioDBContext done

--- TaskManagment.Infrastructure/Data/ApplicationDbContext.cs
+++ TaskManagment.Infrastructure/Data/ApplicationDbContext.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagment.Domain.Entities;
 
-namespace TaskManagment.Infrastructure.Persistence
+namespace TaskManagment.Infrastructure.Data
 {
-    internal class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
         
+         public DbSet<TaskItem> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
