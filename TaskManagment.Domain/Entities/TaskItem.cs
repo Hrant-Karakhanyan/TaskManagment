c7dc70c301f9ae6bac2e590918c68b7188ae1d6a
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-09 07:44:36 -0800

    Initial commit - added Domain

---
+++ TaskManagment.Domain/Entities/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagment.Domain.Entities;
+using TaskManagment.Domain.Enums;
+
+
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public TaskStatus Status { get; set; } = TaskStatus.Pending;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
---
+++ TaskManagment.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagment.Domain.Entities;
+
+
+namespace TaskManagment.Domain.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task<TaskItem?> GetByIdAsync(int id);
+        Task AddAsync(TaskItem task);
+        Task UpdateAsync(TaskItem task);
+        Task DeleteAsync(int id);
+    }
+}
