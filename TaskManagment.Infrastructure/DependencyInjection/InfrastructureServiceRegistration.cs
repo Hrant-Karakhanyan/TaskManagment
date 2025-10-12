89239814a9028db22b609ac6f17a7bffb875dcb0
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-12 01:06:07 -0800

    InfrastructureRegistration and TaskRepo done

--- TaskManagment.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
+++ TaskManagment.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagment.Domain.Interfaces;
+using TaskManagment.Infrastructure.Data;
+using TaskManagment.Infrastructure.Repositories;
 
 namespace TaskManagment.Infrastructure.DependencyInjection
 {
-    internal class InfrastructureServiceRegistration
+    public static class InfrastructureServiceRegistration
     {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ITaskRepository, TaskRepository>();
+            
+            return services;
+        }
     }
 }
--- TaskManagment.Infrastructure/Repositories/TaskRepository.cs
+++ TaskManagment.Infrastructure/Repositories/TaskRepository.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagment.Domain.Entities;
+using TaskManagment.Domain.Interfaces;
+using TaskManagment.Infrastructure.Data;
 
 namespace TaskManagment.Infrastructure.Repositories
 {
-    internal class TaskRepository
+    public class TaskRepository : ITaskRepository
     {
+        private readonly ApplicationDbContext _context;
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(int id)
+        {
+            return await _context.Tasks.FindAsync(id);
+        }
+
+        public async Task AddAsync(TaskItem task)
+        {
+            await _context.Tasks.AddAsync(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TaskItem task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task != null)
+            {
+                _context.Tasks.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
