ebef42fbecdc0c79ff92c0c94b3eb4dc16cea838
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-12 02:03:21 -0800

    app service registration described

--- TaskManagment.Application/DependencyInjection/ApplicationServiceRegistration.cs
+++ TaskManagment.Application/DependencyInjection/ApplicationServiceRegistration.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace TaskManagment.Application.DependencyInjection
 {
-    public static class ApplicationServiceRegistration 
+    public static class ApplicationServiceRegistration
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            
+            // Регистрируем MediatR — автоматически находит все команды, запросы и хендлеры
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
             return services;
         }
     }
