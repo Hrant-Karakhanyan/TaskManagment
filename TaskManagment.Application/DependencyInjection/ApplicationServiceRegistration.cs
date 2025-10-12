850580b965dfa41bbeefd5db81bf9c627d4bb1ed
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-12 01:22:08 -0800

    Application service registration added

---
+++ TaskManagment.Application/DependencyInjection/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagment.Application.DependencyInjection
+{
+    public static class ApplicationServiceRegistration 
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
