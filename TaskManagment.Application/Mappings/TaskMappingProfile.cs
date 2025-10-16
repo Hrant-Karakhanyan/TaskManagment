7d297736fbde84614fcdc3d72db6f47d6aa7fcaf
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-16 07:43:56 -0800

    Task Mapping done

---
+++ TaskManagment.Application/Mappings/TaskMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagment.Application.DTOs;
+using TaskManagment.Domain.Entities;
+
+namespace TaskManagment.Application.Mappings
+{
+    public class TaskMappingProfile:Profile
+    {
+        public TaskMappingProfile() 
+        {   //enum -> string
+            CreateMap<TaskItem, TaskDto>()
+               .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString())); 
+            //string -> enum
+            CreateMap<TaskDto, TaskItem>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Enum.Parse<TaskStatus>(src.Status))); 
+        }
+    }
+}
