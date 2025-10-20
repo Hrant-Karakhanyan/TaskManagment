a5a28f04d8c27d32aa9335f5534f3ad4c3de3f01
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-20 13:41:29 -0800

    Initial commit

---
+++ TaskManagment.API/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagment.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+
+    }
+}
---
+++ TaskManagment.API/Program.cs
@@ -0,0 +1,39 @@
+﻿using TaskManagment.Application.DependencyInjection;
+using TaskManagment.Infrastructure.DependencyInjection;
+
+namespace TaskManagment.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Adding DI from Application and Infrastructure
+            builder.Services.AddApplicationServices();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+
+            // Adding Controllers and Swagger
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Middleware configuration
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            // controller routing
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
