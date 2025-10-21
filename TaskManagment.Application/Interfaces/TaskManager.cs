1b12491ba9f3ef8c87d3824cf44600d48ff87c50
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-10-21 08:24:40 -0800

    Logic Described

---
+++ TaskManagment.Application/Interfaces/TaskManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagment.Application.Interfaces
+{
+    public interface TaskManager
+    {
+        Task CreateTask(string name, string description);
+
+        void DeleteTask(Task task);
+
+        void UpdateTask(Task task, string description);
+
+        Task GetTask(string name);
+
+        List<Task> GetAllTasks(); 
+    }
+}
