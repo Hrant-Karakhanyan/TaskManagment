164a9870991ee54ecad71466411b231e83211bce
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-11-07 08:21:31 -0800

    DBContext created

---
+++ TaskManagment.Infrastructure/Persistance/TaskDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagment.Domain.Entity; 
+
+namespace TaskManagment.Infrastructure.Persistence;
+
+public sealed class TaskDbContext : DbContext
+{
+
+    public DbSet<TaskEntity> Tasks { get; set; } // Имя таблицы будет "Tasks"
+
+    // 2. Оставляем конструктор для DI
+    public TaskDbContext(DbContextOptions<TaskDbContext> options) :
+        base(options){ }
+}
