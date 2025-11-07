ef7c13eaaa3e6dc28e5e544e2b26415ada9fe2e2
Hrant Karakhanyan <hrant.karakhanyan@edu.ysu.am>
2025-11-07 08:26:41 -0800

    TaskRepository created

---
+++ TaskManagment.Infrastructure/Repository/TaskRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagment.Domain.Entity;
+using TaskManagment.Domain.Interfaces;
+using TaskManagment.Infrastructure.Persistence;
+
+namespace TaskManagment.Infrastructure.TaskRepository;
+public class TaskRepository(TaskDbContext context) : ITaskRepository
+{
+    public void Add(TaskEntity task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Guid id, TaskEntity task)
+    {
+        throw new NotImplementedException();
+    }
+
+        
+    public void Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<TaskEntity> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public TaskEntity GetById(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+   
+}
